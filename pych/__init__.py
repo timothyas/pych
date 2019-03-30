618160c45db62ec3ff0c2c3a85fede2fb68d969e
timothys-ekman <timsmith204@utexas.edu>
2019-03-29 20:01:14 -0500

    Add recently made functions

--- pych/__init__.py
+++ pych/__init__.py
@@ -1,5 +1,5 @@
-from .plot import diag_plot
+from .plot import horizontal_map, depth_slice
 
-from .calc import calc_vertical_avg, calc_baro_stf, calc_vel_at_mxl
+from .calc import calc_vertical_avg, calc_baro_stf, calc_overturning_stf, calc_vel_at_mxl
 
 from .readGrdchk import read_grdchk_from_stdout
