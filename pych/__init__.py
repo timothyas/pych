00a99cfb8bb2d1e979e4849c89ddb4662c996920
Timothy Smith <timsmith204@utexas.edu>
2019-07-23 10:02:27 +0200

    Added all statement

--- pych/__init__.py
+++ pych/__init__.py
@@ -1,5 +1,10 @@
 from .plot import horizontal_map, depth_slice
 
-from .calc import calc_vertical_avg, calc_baro_stf, calc_overturning_stf, calc_vel_at_mxl
+from .calc import (
+        haversine, calc_vertical_avg, 
+        calc_baro_stf, calc_overturning_stf, 
+        calc_vel_at_mxl)
 
 from .readGrdchk import read_grdchk_from_stdout
+
+__all__ = ['plot','calc','readGrdchk']
