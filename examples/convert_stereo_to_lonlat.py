4293c7d1aac5ff3715c0a688836b0636d833a546
Timothy Smith <timsmith204@utexas.edu>
2020-03-09 17:38:48 -0500

    convert stereographic to lon/lat...

---
+++ examples/convert_stereo_to_lonlat.py
@@ -0,0 +1,28 @@
+"""
+Use pyproj and cartopy to convert South Polar Stereographic to lat lon
+"""
+
+import numpy as np
+from pyproj import Proj, transform
+import cartopy.crs as ccrs
+
+# define starting projection
+inProj = Proj(ccrs.SouthPolarStereo())
+outProj = Proj(init='epsg:4326') #<- regular latlon
+
+# Assume ds has dataset with usual xmitgcm coordinates... 
+# except XC,YC are not lon,lat
+# they are in weird stereographic projection coordinates (m)
+xc,yc = np.meshgrid(ds.XC.values,ds.YC.values)
+
+
+# Sometimes there is an offset in the stereographic units
+# if it's in lat/lon, then this goes into the ccrs call above
+# e.g. for this Dotson/Crosson domain it's
+x_offset = -1703000
+y_offset = -733000
+xc += x_offset
+yc += y_offset
+
+# finally, do conversion
+lon,lat = transform(inProj,outProj,xc,yc)
